d82662b52e30ad63957ab97375ff3eefc6443f1c
yusts <stasivyura@gmail.com>
2020-02-13 13:59:38 +0100

    Updates

---
+++ precache-manifest.5a306cf20f2c24c7c27541ad8360783e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a40c9b3db15a7da353f7ba8c8bbaf6c",
+    "url": "/api-itunes-search/index.html"
+  },
+  {
+    "revision": "84150deb884a8ae6394c",
+    "url": "/api-itunes-search/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e7d434fc06da42639724",
+    "url": "/api-itunes-search/static/css/main.90957eee.chunk.css"
+  },
+  {
+    "revision": "84150deb884a8ae6394c",
+    "url": "/api-itunes-search/static/js/2.a8dcc404.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/api-itunes-search/static/js/2.a8dcc404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7d434fc06da42639724",
+    "url": "/api-itunes-search/static/js/main.1e21a50b.chunk.js"
+  },
+  {
+    "revision": "6c20111d37fb5ac5ba6d",
+    "url": "/api-itunes-search/static/js/runtime-main.4b978df1.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/api-itunes-search/precache-manifest.5a306cf20f2c24c7c27541ad8360783e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/api-itunes-search/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
